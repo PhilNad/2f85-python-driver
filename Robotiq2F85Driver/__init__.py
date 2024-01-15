a964437ce71cde86ed1f9f75bdc10bbe78e49109
Philippe Nadeau <philippe.nadeau@robotics.utias.utoronto.ca>
2024-01-15 09:03:07 -0500

    Reworked the structure of the package

---
+++ Robotiq2F85Driver/__init__.py
@@ -0,0 +1,6 @@
+from .robotiq_modbus_controller.request import Request as RobotiqModbusRtuRequest
+from .robotiq_modbus_controller.status import Status as RobotiqModbusRtuStatus
+from .robotiq_modbus_controller.driver import RobotiqModbusRtuDriver
+
+__author__ = "Philippe Nadeau"
+__email__ = "philippe.nadeau@robotics.utias.utoronto.ca"
